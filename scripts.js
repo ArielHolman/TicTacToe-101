d836621dbddff2227bbddc5395624a0720878981
ArielHolman <ArielARodrigues01@gmail.com>
2020-06-24 11:06:58 -0500

    updated restart button so it does not save the values when entering new game

--- scripts.js
+++ scripts.js
@@ -1,27 +1,10 @@
-
-//       ***********************
-//            INSTRUCTIONS
-//       ***********************
-
-// 1. Read the code below one block at a time.
-// 2. Look for the @TODOs, and figure out how to fix them.
-    // next to each @TODO you will find tasks that need to be finished
-
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
 let currentMarker = 'X'
-let board = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""]
-]
-
-
+let board = [["", "", ""],["", "", ""],["", "", ""]]
 
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
-
-  // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
   console.log(`The element you clicked on has an id:  ${element.id}`)
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
@@ -35,26 +18,12 @@ const handleClick = (element) => {
 const addMarker = (id) => {
   const row = parseInt(id.charAt(0))
   const column = parseInt(id.charAt(2))
-
   board[row][column] = currentMarker
 
-  console.log (board)
-  // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
-  // console.log(`*** The current marker is:  ${currentMarker}. ***`)
-  // console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
-  
-  // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
   document.getElementById(id).innerHTML= currentMarker
-  // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // = currentMarker
-  // .getElementById(id)
-  // document
-  // .innerHTML 
 
   checkForWin()
 }
-
- 
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
@@ -65,20 +34,10 @@ const changeMarker = () => {
   }
 }
 
-
-
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
-  
-  // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-      const squares = document.getElementsByTagName("TD")
-  // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // squares
-  // .getElementsByTagName("TD")
-  // =
-  // document
-  // const
+  board = [["", "", ""],["", "", ""],["", "", ""]]
+    const squares = document.getElementsByTagName("TD")
   
   // loops over the HTML Collection of TDs and clears out the Xs and Os
   for (i=0; i < squares.length; i++) {
@@ -90,8 +49,6 @@ const resetBoard = () => {
     squares[i].innerHTML = null
   }  
 }
-
-// board[row][column] 
 
 const checkForWin = () => {
   if(horizontalWin() || verticalWin() || diagonalWin()) {
@@ -131,6 +88,6 @@ const diagonalWin = () => {
       } else if ((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")){
         return true
           } else {
-            return false
+              return false
           }
 }
